aec4e0ea235c88303a9e21f5a993be0fa18a419f
Gisan Geff <ggranraniego@gmail.com>
2022-06-29 12:13:21 +0800

    Add notifications migration

---
+++ database/migrations/2022_06_29_041029_create_notifications_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->longText('content')->nullable();
+            $table->enum('status', ['Unread', 'Read'])->default('Unread');
+            $table->tinyInteger('active')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifications');
+    }
+}
