b65859c2f0fa7809ea0c8f9745a4d3ee9a842e16
Gisan Geff <ggranraniego@gmail.com>
2022-06-29 12:23:17 +0800

    Add college users migration

---
+++ database/migrations/2022_06_29_042122_create_college_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCollegeUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('college_users', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('college_id');
+            $table->bigInteger('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('college_users');
+    }
+}
---
+++ database/migrations/2022_06_29_042219_create_college_action_plans_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCollegeActionPlansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('college_action_plans', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('college_action_plans');
+    }
+}
