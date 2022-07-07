cd9a888d6c25a426b8b576a5ed53102aea1c978b
Gisan Geff <ggranraniego@gmail.com>
2022-07-08 03:41:31 +0800

    Add College model

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\College;
 use App\Models\CollegeUser;
 use App\Models\Program;
 use App\Models\ProgramUser;
--- app/Models/College.php
+++ app/Models/College.php
@@ -5,14 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Program extends Model
+class College extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'college_id',
         'name',
-        'description',
-        'is_institutional'
+        'description'
     ];
 }
